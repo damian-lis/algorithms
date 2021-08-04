a0be3e8847c5e5d95832cc632055f68b94641941
Damian Lis <foxprogramming93@gmail.com>
2021-08-04 14:53:55 +0200

    Created the binary search algorithm

---
+++ binarySearch.js
@@ -0,0 +1,33 @@
+// Is better than linear search algo
+// Linear search is working with ordered and unordered list
+// Binary search is working only with the ordered list
+// In this case we splitour array every time and get result
+
+const binarySearch = (arr, element) => {
+  let startIndex = 0;
+  let endIndex = arr.length - 1;
+
+  while (startIndex <= endIndex) {
+    const middleIndex = Math.floor((endIndex - startIndex) / 2);
+
+    if (arr[middleIndex] === element) {
+      return middleIndex;
+    }
+
+    if (arr[middleIndex] > element) {
+      endIndex = middleIndex - 1;
+    } else {
+      startIndex = middleIndex + 1;
+    }
+  }
+};
+
+const arr = [1, 3, 5, 6, 7, 8, 99];
+
+console.log(binarySearch(arr, 6));
+
+// Best case: searching number exist in the middle of array => O(1)
+// Average case: O(log n)
+// Worst case: O(log n)
+
+// On every iteration we have split our passed data. so we have logaritmic time complexity
