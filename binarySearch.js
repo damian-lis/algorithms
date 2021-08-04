80213ed1ad130fa379c8588b798244559cfe4b4f
Damian Lis <foxprogramming93@gmail.com>
2021-08-04 15:23:23 +0200

    Created the recursive version of binary search algorithm

--- binarySearch.js
+++ binarySearch.js
@@ -30,3 +30,33 @@ console.log(binarySearch(arr, 6));
 // Worst case: O(log n)
 
 // On every iteration we have split our passed data. so we have logaritmic time complexity
+
+
+
+// Recursive Binary Search:
+
+const recursiveBinarySearch = (arr, element, offset) => {
+    let startIndex = 0;
+    let endIndex = arr.length - 1;
+  
+    const middleIndex = Math.floor((endIndex - startIndex) / 2);
+  
+    if (arr[middleIndex] === element) {
+        return middleIndex + offset;
+    }
+  
+    if (arr[middleIndex] > element) {
+        endIndex = middleIndex;
+    } else {
+        startIndex = middleIndex + 1;
+        offset = offset + middleIndex + 1
+    }
+
+    return recursiveBinarySearch(arr.slice(startIndex, endIndex + 1), element, offset) 
+  };
+  
+  const arr2 = [1, 3, 5, 6, 7, 8, 99];
+  
+  console.log(recursiveBinarySearch(arr2, 3, 0));
+
+//  Offset as info for array subset for remember the previous index of the array (this is only the case when we have the second half of array - here we lost correct index number)
