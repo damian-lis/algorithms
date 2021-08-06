24c718371617d8e9713a11134ce770660eb462d7
Damian Lis <foxprogramming93@gmail.com>
2021-08-06 12:23:44 +0200

    Improved the knapsack algorithm by adding the memoization

--- knapsack.js
+++ knapsack.js
@@ -1,22 +1,28 @@
-const knapsackFn = (items, cap, itemIndex) => {
+const knapsackFn = (items, cap, itemIndex, mem) => {
+  if (mem[cap][itemIndex]) {
+    return mem[cap][itemIndex];
+  }
   if (cap === 0 || itemIndex < 0) {
     return { items: [], value: 0, weight: 0 };
   }
 
   if (cap < items[itemIndex].weight) {
-    return knapsackFn(items, cap, itemIndex - 1);
+    return knapsackFn(items, cap, itemIndex - 1, mem);
   }
 
   const sackWithItem = knapsackFn(
     items,
     cap - items[itemIndex].weight,
-    itemIndex - 1
+    itemIndex - 1,
+    mem
   );
 
-  const sackWithoutItem = knapsackFn(items, cap, itemIndex - 1);
+  const sackWithoutItem = knapsackFn(items, cap, itemIndex - 1, mem);
 
   const valueWithItem = sackWithItem.value + items[itemIndex].value;
   const valueWithoutItem = sackWithItem.value;
+
+  let resultSack;
 
   if (valueWithItem > valueWithoutItem) {
     const updatedSack = {
@@ -25,10 +31,22 @@ const knapsackFn = (items, cap, itemIndex) => {
       weight: sackWithItem.weight + items[itemIndex].weight,
     };
 
-    return updatedSack;
+    resultSack = updatedSack;
   } else {
-    return sackWithoutItem;
+    resultSack = sackWithoutItem;
   }
+
+  mem[cap][itemIndex] = resultSack;
+
+  return resultSack;
+};
+
+// Its a knapsack wrapper
+const knapsack = (items, cap, index) => {
+  const mem = Array.from(Array(cap + 1), () => Array(items.length)).fill(
+    undefined
+  );
+  return knapsackFn(items, maxCap, items, mem);
 };
 
 const items = [
@@ -39,7 +57,7 @@ const items = [
 
 const maxCap = 8;
 
-console.log(knapsackFn(items, maxCap, items.length - 1));
+console.log(knapsack(items, maxCap, items.length - 1));
 
 // In the worst case we have two recursive calls sackWithItem and sackWithoutItem => O(2^n)
-
+// After improvement by adding dynamic solution with memo => O(n * C) / C - capacity (becouse we depend on n and C)
